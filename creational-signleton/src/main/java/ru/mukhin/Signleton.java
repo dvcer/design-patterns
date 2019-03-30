1d79ab14ffd29ac7e35d57b68ecd5a0832866be3
dvcer <am31101990@gmail.com>
2019-03-30 13:12:00 +0200

    + Singleton

--- creational-signleton/src/main/java/ru/mukhin/Signleton.java
+++
@@ -1,5 +0,0 @@
-package ru.mukhin;
-
-public class Signleton {
-    
-}
---
+++ creational-signleton/src/test/java/SingletonTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+import ru.mukhin.Singleton;
+
+public class SingletonTest {
+    @Test
+    public void signletonTest() {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Assert.assertEquals(s1, s2);
+    }
+}
